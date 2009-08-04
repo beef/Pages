aab65de67a1e8ecb9ce64388be74c2d86ae84f65
Steve England <steve@wearebeef.co.uk>
2009-08-04 12:35:17 +0100

    Adding pages index path so page_url methofd is created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :pages, :only => :show, :collection => {:preview => :get}
+  map.resources :pages, :only => :show, :index, :collection => {:preview => :get}
   map.namespace(:admin) do |admin|
     admin.resources :pages, :has_many => :pages, :collection => {:preview => :post}, :member => {:preview => :put, :feature => [:get, :put], :move_up => :get, :move_down => :get}
   end
