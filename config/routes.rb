e91f9d5aab33b8bf6a776ced8d4385982ff05a04
Daniel Craig <daniel@wearebeef.co.uk>
2009-08-27 14:38:52 +0100

    removed pages route, must be set by application

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ ActionController::Routing::Routes.draw do |map|
   map.namespace(:admin) do |admin|
     admin.resources :pages, :has_many => :pages, :collection => {:preview => :post}, :member => {:preview => :put, :feature => [:get, :put], :move_up => :get, :move_down => :get}
   end
-  map.page ':id', :controller => 'pages', :action => 'show'
 end
