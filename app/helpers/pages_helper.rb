2c25af1f0a0d3727e8bd4e8fe7f10025dbc0cb7f
Steve England <steve@wearebeef.co.uk>
2009-09-01 16:23:10 +0100

    Adding related pages helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,7 @@
 module PagesHelper
+  def related_pages(taggable, limit = 3, &block)
+    pages = Page.published.all(taggable.related_search_options(:tags, Page, :limit => limit))
+    return if pages.empty?
+    yield(pages)
+  end
 end
