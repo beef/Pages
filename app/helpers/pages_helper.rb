cee9d10f466f1855c9e3341ec3f1ab6104520fff
Daniel Craig <daniel@wearebeef.co.uk>
2009-09-02 17:22:43 +0100

    changed admin index view and crete subpage helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -4,4 +4,15 @@ module PagesHelper
     return if pages.empty?
     yield(pages)
   end
+  
+  
+  def sub_pages_list(permalink)
+    page = Page.find_by_permalink(permalink)
+    return if page.nil?
+    page_items = page.children.published.collect do |sub_page|
+      content_tag( :li, link_to( h(sub_page.title), page_path(sub_page.permalink)))
+    end
+    content_tag :ul, page_items.join unless page_items.empty? 
+  end
+
 end
