c8f1ea4e330b5f25718ec5549eb5ad4e9d46e94a
Steve England <steve@wearebeef.co.uk>
2009-09-10 11:24:47 +0100

    Template ordering fix

--- lib/pages.rb
+++ lib/pages.rb
@@ -3,12 +3,15 @@ module Beef
     module HelperMethods
       def get_template_names
         templates = []
-        Dir.glob("#{RAILS_ROOT}/app/views/pages/templates/*"){|f| match = /\/([^\/]+)\.html\.erb$/.match(f); templates << match[1] unless match.nil? }
+        Dir.glob("#{RAILS_ROOT}/app/views/pages/templates/*") do |f| 
+          match = /\/([^\/]+)\.html\.erb$/.match(f)
+          templates << match[1] unless match.nil?
+        end
         # Move default to top if it exists
         if default = templates.delete('default')
           templates.insert(0, default)
         end 
-        templates.sort
+        templates
       end
     end
   end
