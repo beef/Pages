d625f35b6b4ec3cb74e5d957224e29702cb09afe
Steve England <steve@wearebeef.co.uk>
2010-01-19 17:47:24 +0000

    Make preview route more important than permalink

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
-  map.page_permalink 'pages/:id.:format', :controller => 'pages', :action => 'show'
   map.preview_pages 'pages/preview.:format', :controller => 'pages', :action => 'preview'
+  map.page_permalink 'pages/:id.:format', :controller => 'pages', :action => 'show'
   map.namespace(:admin) do |admin|
     admin.resources :pages, :has_many => :pages, :collection => {:preview => :post}, :member => {:preview => :put, :move_up => :get, :move_down => :get}
   end
